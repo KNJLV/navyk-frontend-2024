35ee2935c28f6d3a6345d2cb26dd1c4bfdeadc8e
Alexander K <sashakinzalov_2002@mail.ru>
2024-01-30 17:08:53 +0300

    add alert pic

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
-const studentButton = document.getElementById("contactFormStudentButton");
+// Слайдер в форме - Родитель
 const parentButton = document.getElementById("contactFormParentButton");
+
+// Слайдер в форме - Ученик
+const studentButton = document.getElementById("contactFormStudentButton");
+
+// Имя ученика в форме
 const studentName = document.getElementById("studentName");
+
+// Номер ученика в форме
 const studentPhoneNumber = document.getElementById("studentPhoneNumber");
+
+// Кнопка отправки в форме
+const button = document.querySelector(".section-form__block-left__button");
+
+// Чекбокс в форме
+const chekedPersonalData = document.querySelector(".checkbox");
+
+// Имя родителя в форме
+const parentName = document.querySelector("#parentName");
+
+// Номер родителя в форме
+const parentPhoneNumber = document.querySelector("#parentPhoneNumber");
+
+// ====================================================
 
 // я не знаю что это :D
 document.addEventListener("DOMContentLoaded", () => {
@@ -10,7 +31,9 @@ document.addEventListener("DOMContentLoaded", () => {
   studentPhoneNumber.style.display = "none";
 });
 
-// переключение по кнопке РОДИТЕЛЬ
+// ====================================================
+
+// Переключение по кнопке РОДИТЕЛЬ
 parentButton.addEventListener("change", () => {
   let inputsBlock = document.querySelector(".section-form__block-left-inputs");
   console.log(inputsBlock);
@@ -25,7 +48,7 @@ parentButton.addEventListener("change", () => {
   }
 });
 
-// переключение по кнопке УЧЕНИК
+// Переключение по кнопке УЧЕНИК
 studentButton.addEventListener("change", () => {
   let inputsBlock = document.querySelector(".section-form__block-left-inputs");
   console.log(inputsBlock);
@@ -40,13 +63,9 @@ studentButton.addEventListener("change", () => {
   }
 });
 
-const buttons = document.querySelector(".section-form__block-left__button");
-const chekedPersonalData = document.querySelector(".checkbox");
-const parentName = document.querySelector("#parentName");
-const parentPhoneNumber = document.querySelector("#parentPhoneNumber");
-const parentEmail = document.querySelector("#parentEmail");
+// ====================================================
 
-// функция отправки данных из формы в бота
+// Функция отправки данных из формы
 function SendFormInfo() {
   let klientInformation = document.querySelectorAll(
     ".section-form__block-left__info-line"
@@ -74,111 +93,128 @@ function SendFormInfo() {
   klientInformation.forEach((e) => {
     e.value = "";
   });
-  alert("Заявка успешно отправлена!");
 }
 
-// Отправка формы НАЧАЛО
-buttons.addEventListener("click", (e) => {
+// ====================================================
+
+// Функция по нажатию кнопки "Оставить заявку"
+button.addEventListener("click", (e) => {
   e.preventDefault();
-  // проверка полей при заполнение РОДИТЕЛЕМ
+  // Проверка полей при заполнение РОДИТЕЛЕМ
   if (parentButton.checked) {
     if (
-      // проверка чекбокса и полей родителя (когда всё заполнено)
+      // Проверка чекбокса и полей родителя (когда всё заполнено)
       chekedPersonalData.checked &&
       parentName.value != "" &&
       parentPhoneNumber.value != ""
-      //   && parentEmail.value != ""
     ) {
       SendFormInfo();
-	  backgroundStyleChange();
+      backgroundStyleChange();
     } else if (
-      // проверка полей родителя (когда не заполнены поля)
+      // Проверка полей родителя (когда не заполнены поля)
       parentName.value == "" ||
       parentPhoneNumber.value == ""
-      // || parentEmail.value == ""
     ) {
       alert("Заполнены не все обязательные поля");
     } else if (!chekedPersonalData.checked) {
-      // проверка чекбокса (когда он не нажат)
+      // Проверка чекбокса (когда он не нажат)
       alert("Вы не приняли условия обработки персональных данных");
     }
 
-    // проверка полей при заполнение УЧЕНИКОМ
+    // Проверка полей при заполнение УЧЕНИКОМ
   } else if (studentButton.checked) {
     if (
-      // проверка чекбокса и полей родителя (когда всё заполнено)
+      // Проверка чекбокса и полей родителя (когда всё заполнено)
       chekedPersonalData.checked &&
       parentName.value != "" &&
       parentPhoneNumber.value != "" &&
-      //   && parentEmail.value != ""
       studentName.value != "" &&
       studentPhoneNumber.value != ""
     ) {
       SendFormInfo();
-	  backgroundStyleChange();
+      backgroundStyleChange();
     } else if (
-      // проверка полей родителя (когда не заполнены поля)
+      // Проверка полей родителя (когда не заполнены поля)
       parentName.value == "" ||
       parentPhoneNumber.value == "" ||
-      //   parentEmail.value == "" ||
       studentName.value == "" ||
       studentPhoneNumber.value == ""
     ) {
       alert("Заполнены не все обязательные поля");
     } else if (!chekedPersonalData.checked) {
-      // проверка чекбокса (когда он не нажат)
+      // Проверка чекбокса (когда он не нажат)
       alert("Вы не приняли условия обработки персональных данных");
     }
   }
 });
-// Отправка формы КОНЕЦ
-function backgroundStyleChange()
-{
-	let a = document.querySelector(".good__attention");
-	a.innerHTML = `<div class="modal_window" style="
+
+// ====================================================
+
+// Функция появления алерта об отправке формы
+function backgroundStyleChange() {
+  let a = document.querySelector(".good__attention");
+  a.innerHTML = `<div class="modal_window" style="
       width: 100%;
       height: 100%;
       position: fixed;
       top: 0px;
       left: 0px;
-      background-color: rgba(0, 0, 0, 0.3);
+      background-color: rgba(0, 0, 0, 0.4);
+      backdrop-filter: blur(2.5px);
       z-index: 999;
       ">
 	  
       <div class="model_form" style="
       display:flex;
-      width: 100%;
+      width: 90%;
       height: 100%;
       align-items: center;
       justify-content: center;
+      margin: auto;
     ">
 	
 	<div class="scroll" style="position: relative">
-	<img src="https://grantara.ru/wp-content/uploads/2023/06/image16-2.jpg" />
+	<img src="./assets/section-form/ALERT-BANNER.png" />
 	<div class="modalClose" style="
       position: absolute;
       cursor: pointer;
-      top: 20px;
-      right: 20px;
-      font-size: 32px;
+      top: 30px;
+      right: 40px;
+      font-size: 40px;
 	">✕</div>
 
 	</div>
 	</div>
 	</div>`;
-let close_attention = document.querySelector(".modalClose");
-close_attention.addEventListener("click", (e) => {
-	let modal_window = document.querySelector(".modal_window");
-	modal_window.style.display="none";
-})
+  let close_attention = document.querySelector(".modalClose");
+  close_attention.addEventListener("click", (e) => {
+    let modal_window = document.querySelector(".modal_window");
+    modal_window.style.display = "none";
+  });
 }
-let courses_buttons = document.querySelectorAll(".section-courses__card-button-2");
-let courses_name = document.querySelectorAll(".section-courses__card-header-left-title")
-for(let i = 0; i<courses_buttons.length; i++){
-courses_buttons[i].addEventListener("click", (e) => {
-	let id_pole = document.querySelector("#formComment");
-	if(i!=3){
-	id_pole.value=`${courses_name[i].textContent} КУРС`;
-	}
-	else if(i==3){id_pole.value=`ОНЛАЙН-ЛЕКТОРИЙ`;}
-})}
+
+// ====================================================
+
+// Кнопки в курсах "Записаться"
+let courses_button = document.querySelectorAll(
+  ".section-courses__card-button-2"
+);
+
+// Названия курсов
+let courses_name = document.querySelectorAll(
+  ".section-courses__card-header-left-title"
+);
+
+// ====================================================
+
+// Перенос названия курса в комментарий формы
+for (let i = 0; i < courses_button.length; i++) {
+  courses_button[i].addEventListener("click", (e) => {
+    let id_pole = document.querySelector("#formComment");
+    if (i != 3) {
+      id_pole.value = `${courses_name[i].textContent} КУРС`;
+    } else if (i == 3) {
+      id_pole.value = `ОНЛАЙН-ЛЕКТОРИЙ`;
+    }
+  });
+}
